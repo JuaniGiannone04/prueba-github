465c76e88ad94a0b9c61b5585af112380ab2d590
JuaniGiannone04 <jgiannone@fi.uba.ar>
2026-04-03 11:24:05 -0300

    Agrego archivo final de errores

--- Bash-Regex/validos.sh
+++ Bash-Regex/validos.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # ESTE SCRIPT LISTA LOS USUARIOS QUE TENGAN UN FORMATO VALIDO: USERNAME SIN ESPACIOS
-# Y CONTRASEÑAS CON NUMERO
+# Y CONTRASEÑAS CON NUMERO. ADEMAS GUARDA EN UN ARCHIVO DE ERRORES LOS USUARIOS 
+# Y CONTRASEÑAS QUE NO CUMPLAN
 
 grep -E "^[^, ]+,[^,]*[0-9][^,]*$" usuarios.csv
 
+grep -v "^[^, ]+,[^,]*[0-9][^,]*$" usuarios.csv > errores.csv
